59fde5c01dcf3b31a74036655b7f304044996198
eligosleyak <shashankchr@protonmail.ch>
2024-08-08 19:01:10 +0545

    Even or Odd

---
+++ controlAndFunctions/basics.py
@@ -0,0 +1,14 @@
+'''
+Conditional statement
+    - Statements where a certain part of code is executed when a condition is satisfies
+    -Syntax
+        if (condition1.1) and (condition1.2) or (condition2):
+            satisfied function()
+        else:
+            Unsatisfied funtion()
+'''
+x = 10
+if (x%2 == 0):
+    print("Even Number")
+else:
+    print("Odd Number")
