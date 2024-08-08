1af13c72f8d48de774c3f65bad774c7248fe4128
eligosleyak <shashankchr@protonmail.ch>
2024-08-08 19:41:13 +0545

    Loop theory and one example with range()

--- controlAndFunctions/basics.py
+++ controlAndFunctions/basics.py
@@ -39,30 +39,43 @@ Conditional statement
 #     print(f'Username not found')
 
 # Task 2: Create a KBC / quiz where 5 questions are asked and 5 answers need to be provided after each correct answer score increases by 100
-score = 0
-ans = input('What country has the highest life expectancy?')
-if ans.lower() == 'hong kong':
-    score+=100
-    ans = input('Where would you be if you were standing on the Spanish Steps?')
-    if ans.lower() == 'rome':
-        score+=100
-        ans = input('Which language has the more native speakers: English or Spanish? ')
-        if ans.lower() == 'spanish':
-            score+=100
-            ans = input('What is the most common surname in the United States?')
-            if ans.lower() == 'smith':
-                score+=100
-                ans = input('What disease commonly spread on pirate ships? ')
-                if ans.lower() == 'scurvy':
-                    score+=100
-                    print(f'Successfully answered all questions score is {score}')
-                else:
-                    print(f'You got {score}')
-            else:
-                print(f'You got {score}')
-        else:
-            print(f'You got {score}')
-    else:
-     print(f'You got {score}')
-else:
-    print(f'You got {score}')
+# score = 0
+# ans = input('What country has the highest life expectancy?')
+# if ans.lower() == 'hong kong':
+#     score+=100
+#     ans = input('Where would you be if you were standing on the Spanish Steps?')
+#     if ans.lower() == 'rome':
+#         score+=100
+#         ans = input('Which language has the more native speakers: English or Spanish? ')
+#         if ans.lower() == 'spanish':
+#             score+=100
+#             ans = input('What is the most common surname in the United States?')
+#             if ans.lower() == 'smith':
+#                 score+=100
+#                 ans = input('What disease commonly spread on pirate ships? ')
+#                 if ans.lower() == 'scurvy':
+#                     score+=100
+#                     print(f'Successfully answered all questions score is {score}')
+#                 else:
+#                     print(f'You got {score}')
+#             else:
+#                 print(f'You got {score}')
+#         else:
+#             print(f'You got {score}')
+#     else:
+#      print(f'You got {score}')
+# else:
+#     print(f'You got {score}')
+
+'''
+Looop and iterations:
+    -Repeat same task over and over again
+    -Can be used for various usecases like asking multiple queries, running program indefinetly and so on.
+
+    -for loop and while loop
+        -for loop used whrn we know the starting point and we know the ending point of the loop
+        -while loop used when we don't know the ending point
+'''
+
+for vara in range(1,10):
+    print(vara)
