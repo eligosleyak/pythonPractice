0619451447d42d031498fe8efb89fa8dded49c72
eligosleyak <shashankchr@protonmail.ch>
2024-08-12 19:55:21 +0545

    task 1 incomplete

--- librariesUnderstanding/basics.py
+++ librariesUnderstanding/basics.py
@@ -14,21 +14,40 @@ Pandas Library:
     -Work on table, read table, modify table, save table and everything related table.
 """
 
-dicta = {
-    'student': ['Ram', 'Shyam', 'Sita', 'Gita'],
-    'marks': [10, 20, 30, 40],
-}
+# dicta = {
+#     'student': ['Ram', 'Shyam', 'Sita', 'Gita'],
+#     'marks': [10, 20, 30, 40],
+# }
 
 #Pandas operation 1:
-dataFrameVariable = pd.DataFrame(dicta)
-print(dataFrameVariable)
+# dataFrameVariable = pd.DataFrame(dicta)
+# print(dataFrameVariable)
 
-#Adding new column
-dataFrameVariable['Social Skills'] = [10,20,30,40]
-print('--'*25)
-print(dataFrameVariable)
+# #Adding new column
+# dataFrameVariable['Social Skills'] = [10,20,30,40]
+# print('--'*25)
+# print(dataFrameVariable)
 
-#Removing column
-dataFrameVariable = dataFrameVariable.drop(columns = ('Social Skills'))
-print('--'*25)
-print(dataFrameVariable)
+# #Removing column
+# dataFrameVariable = dataFrameVariable.drop(columns = ('Social Skills'))
+# print('--'*25)
+# print(dataFrameVariable)
+
+'''
+Task 1:
+    Take n from user (N = number of questions)
+    get n number of questions and N number of answers for quiz
+    collect all questions in qtn list and asn in ans list
+    convert to dictionary
+    convet to dataframe
+    display the final table
+'''
+n = int(input('Enter number of questions: '))
+qtn = []
+ans = []
+for i in range(0,n):
+    qtn.append(input('Enter a question: '))
+    ans.append(('Enter the answer: '))
+dic = dict(zip(qtn,ans))
+dataFrame = pd.DataFrame(dic)
+print(dataFrame)
