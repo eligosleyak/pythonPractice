42bda60559e9befadb44a43a545a50c214f7b0b3
eligosleyak <shashankchr@protonmail.ch>
2024-08-14 19:15:41 +0545

    generator

--- advancePythonTopics/basics.py
+++ advancePythonTopics/basics.py
@@ -28,5 +28,27 @@ when complex condition arisies list comprehesion is difficult to code
 # print(even)
 
 #squared and cube
-snq = [i**2 if i%2 == 0 else i**3 for i in range (1,100) ]
-print (snq)
+# snq = [i**2 if i%2 == 0 else i**3 for i in range (1,100) ]
+# print (snq)
+
+'''
+Generator in pyhton
+    -Special type of function that returns multiple value for multiple different instraces using yield
+    -def function_name()
+        yield statement
+'''
+#Simple generator
+def simple_generator():
+    yield 'a'
+    yield 'b'
+
+#Approach 1 to get output from generator
+for value in simple_generator():
+    print (value)
+
+print('--'*25)
+
+#Approch 2
+x = simple_generator()
+print(next(x))
+print(next(x))
