0a17f67c74c4b2797a9c2b931476cfddb89bfe23
eligosleyak <shashankchr@protonmail.ch>
2024-08-18 19:22:18 +0545

    Constructor

--- ObjectOrientdProgramming/basics.py
+++ ObjectOrientdProgramming/basics.py
@@ -30,3 +30,49 @@ species2.whatSoundDoesItMake('lol')
 
 print('--'*50)
 
+'''
+Create a class named calculator, That must have following methods
+add_2_numbers(), subract_2_numbers(), multiply_2_numbers(), divide_2_numbers()
+create 2 different instances of calculator (create 2 methods) and run those methods
+'''
+class calculator:
+    def add_2_numbers(self,x,y):
+        print(f'The sum is {x+y}')
+
+    def subract_2_numbers(self,x,y):
+        print(f'The subtraction is {x-y}')
+        
+    def multiply_2_numbers(self,x,y):
+        print(f'The multiplication is {x*y}')
+
+    def divide_2_numbers(self,x,y):
+        print(f'The division is {x/y}')
+
+sumNsub = calculator()
+mulNdiv = calculator()
+
+sumNsub.add_2_numbers(2,5)
+sumNsub.subract_2_numbers(5,4)
+
+mulNdiv.multiply_2_numbers(3,7)
+mulNdiv.divide_2_numbers(4,2)
+
+print('--'*50)
+
+'''
+Constructor:
+    -Constructor is used during initialization, such that as soon as an object is created it jumps to conclusion part
+    -Syntax for python
+        def __init__(self):
+            Constructor_code_here()
+'''
+
+class KBC:
+    def __init__(self,user):
+        print(f'Welcome to KBC {user}')
+
+GameForSiddhant = KBC('Sidhannt')
+GameForLego = KBC('Lego')
+
+print('--'*50)
+
